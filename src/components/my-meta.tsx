8609e9e14bf7c39eb822bf5aa15cf904e142a4f3
TungLT <letungl2t@gmail.com>
2021-07-17 13:07:33 +0700

    Updated

--- src/components/my-meta.tsx
+++ src/components/my-meta.tsx
@@ -8,6 +8,7 @@ type Props = {
 };
 const MyMeta = ({ title, description, imageUrl, url }: Props) => {
   const titleContent = `edumate | ${title}`;
+  const imgUrl = imageUrl.startsWith('http') ? imageUrl : `https://edumate.vn${imageUrl}`;
   return (
     <Head>
       <title>{titleContent}</title>
@@ -15,11 +16,11 @@ const MyMeta = ({ title, description, imageUrl, url }: Props) => {
       <meta name="description" content={description} />
       <meta name="og:title" content={titleContent} />
       <meta name="og:description" content={description} />
-      <meta name="og:image" content={imageUrl} />
+      <meta name="og:image" content={imgUrl} />
       <meta name="og:url" content={url} />
       <meta name="twitter:title" content={titleContent} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={imageUrl} />
+      <meta name="twitter:image" content={imgUrl} />
       <meta name="twitter:url" content={url} />
     </Head>
   );
