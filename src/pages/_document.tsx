c2f8d99744b4c85a4ebd31a7145f7b341771ea01
TungLT <letungl2t@gmail.com>
2021-08-02 23:20:11 +0700

    Updated PWA config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 import { ColorModeScript } from '@chakra-ui/react';
 
 export default class Document extends NextDocument {
@@ -6,8 +6,18 @@ export default class Document extends NextDocument {
     return (
       <Html>
         <Head>
-          <link rel="icon" type="image/x-icon" href="favicon.ico" />
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover' />
+          <meta name="application-name" content="edumate" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="edumate" />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
+          <meta name="theme-color" content="#fff" />
+
+          <link rel="apple-touch-icon" href="icons/icon-192x192.png"></link>
           <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" type="image/x-icon" href="favicon.ico" />
         </Head>
         <body>
           {/* Make Color mode to persists when you refresh the page. */}
