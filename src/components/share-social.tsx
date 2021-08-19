6d50075eef5375c5184a7dcbcc316ed39a6ba8cd
TungLT <letungl2t@gmail.com>
2021-08-19 22:38:16 +0700

    Only share on desktop

--- src/components/share-social.tsx
+++ src/components/share-social.tsx
@@ -8,6 +8,7 @@ import {
   ModalOverlay,
   Tooltip,
   useDisclosure,
+  useMediaQuery,
 } from '@chakra-ui/react';
 import { Icon } from '@chakra-ui/icons';
 import { BsThreeDots } from 'react-icons/bs';
@@ -18,6 +19,7 @@ type Props = {
 };
 const ShareSocial = ({ path }: Props) => {
   const t = useTranslations('Shared');
+  const [isLargerThan2Md] = useMediaQuery('(min-width: 896px)');
   const [link, setLink] = useState<string>('');
   const [copiedLink, setCopiedLink] = useState<string>('');
   const [fbLink, setFbLink] = useState<string>('');
@@ -39,7 +41,7 @@ const ShareSocial = ({ path }: Props) => {
   };
 
   const openPopupWindow = (isFb = true) => {
-    window.open(isFb ? fbLink : linkedInLink, 'myWindow', 'width=500,height=500');
+    window.open(isFb ? fbLink : linkedInLink, '_blank', 'width=500,height=500');
   };
 
   const onCloseModal = () => {
@@ -49,80 +51,84 @@ const ShareSocial = ({ path }: Props) => {
 
   return (
     <>
-      <Box position={'absolute'} top="0.5em" right="1em" zIndex={1}>
-        <Tooltip hasArrow label={t('title')} aria-label="A tooltip" shouldWrapChildren>
-          <Icon
-            as={BsThreeDots}
-            w={5}
-            h={5}
-            color="gray.400"
-            zIndex={2}
-            onClick={onOpen}
-            cursor="pointer"
-            _hover={{
-              color: 'gray.600',
-            }}
-          />
-        </Tooltip>
-      </Box>
-      <Modal
-        isOpen={isOpen}
-        onClose={onCloseModal}
-        isCentered
-        blockScrollOnMount
-        autoFocus={false}
-        returnFocusOnClose={false}
-      >
-        <ModalOverlay />
-        <ModalContent w="150px">
-          <Flex direction="row" justifyContent="space-around" alignItems="center" p={2}>
-            <Tooltip hasArrow label={t('facebook')} closeOnClick={false} shouldWrapChildren>
+      {isLargerThan2Md && (
+        <Box>
+          <Box position={'absolute'} top="0.5em" right="1em" zIndex={1}>
+            <Tooltip hasArrow label={t('title')} aria-label="A tooltip" shouldWrapChildren>
               <Icon
-                as={AiOutlineFacebook}
-                w={8}
-                h={8}
+                as={BsThreeDots}
+                w={5}
+                h={5}
                 color="gray.400"
+                zIndex={2}
+                onClick={onOpen}
                 cursor="pointer"
-                onClick={() => openPopupWindow()}
                 _hover={{
                   color: 'gray.600',
                 }}
               />
             </Tooltip>
-            <Tooltip hasArrow label={t('linkedIn')} closeOnClick={false} shouldWrapChildren>
-              <Icon
-                as={AiOutlineLinkedin}
-                w={8}
-                h={8}
-                color="gray.400"
-                cursor="pointer"
-                onClick={() => openPopupWindow(false)}
-                _hover={{
-                  color: 'gray.600',
-                }}
-              />
-            </Tooltip>
-            <Tooltip
-              hasArrow
-              label={copiedLink ? t('copied') : t('copy')}
-              closeOnClick={false}
-              shouldWrapChildren
-            >
-              <Icon
-                as={AiOutlineLink}
-                w={8}
-                h={8}
-                color="gray.400"
-                cursor="pointer"
-                onClick={onCopyLink}
-                _hover={{
-                  color: 'gray.600',
-                }}
-              />
-            </Tooltip>
-          </Flex>
-        </ModalContent>
-      </Modal>
+          </Box>
+          <Modal
+            isOpen={isOpen}
+            onClose={onCloseModal}
+            isCentered
+            blockScrollOnMount
+            autoFocus={false}
+            returnFocusOnClose={false}
+          >
+            <ModalOverlay />
+            <ModalContent w="150px">
+              <Flex direction="row" justifyContent="space-around" alignItems="center" p={2}>
+                <Tooltip hasArrow label={t('facebook')} closeOnClick={false} shouldWrapChildren>
+                  <Icon
+                    as={AiOutlineFacebook}
+                    w={8}
+                    h={8}
+                    color="gray.400"
+                    cursor="pointer"
+                    onClick={() => openPopupWindow()}
+                    _hover={{
+                      color: 'gray.600',
+                    }}
+                  />
+                </Tooltip>
+                <Tooltip hasArrow label={t('linkedIn')} closeOnClick={false} shouldWrapChildren>
+                  <Icon
+                    as={AiOutlineLinkedin}
+                    w={8}
+                    h={8}
+                    color="gray.400"
+                    cursor="pointer"
+                    onClick={() => openPopupWindow(false)}
+                    _hover={{
+                      color: 'gray.600',
+                    }}
+                  />
+                </Tooltip>
+                <Tooltip
+                  hasArrow
+                  label={copiedLink ? t('copied') : t('copy')}
+                  closeOnClick={false}
+                  shouldWrapChildren
+                >
+                  <Icon
+                    as={AiOutlineLink}
+                    w={8}
+                    h={8}
+                    color="gray.400"
+                    cursor="pointer"
+                    onClick={onCopyLink}
+                    _hover={{
+                      color: 'gray.600',
+                    }}
+                  />
+                </Tooltip>
+              </Flex>
+            </ModalContent>
+          </Modal>
+        </Box>
+      )}
     </>
   );
 };
