046dfaed4c3c3e45954be9a013c0168c4f14fc49
TungLT <tung.le@niteco.se>
2021-09-06 13:35:40 +0700

    Updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { GetStaticPropsContext } from 'next';
 import NextLink from 'next/link';
 import { useTranslations } from 'next-intl';
-import { Box, Button, Flex, Img, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Img, Text } from '@chakra-ui/react';
 
 import { getHomeContent } from '@/lib/api';
 import markdownToHtml from '@/lib/markdownToHtml';
@@ -42,20 +42,38 @@ const Index = ({ homes }: Props) => {
           w="100%"
           h="100%"
         >
-          <Text
-            fontSize={{ base: '1.5em', md: '1.75em' }}
-            padding="0 0.5em"
-            textTransform="uppercase"
-            color="gray.700"
-            textAlign="center"
+          <Flex
+            direction="column"
+            boxShadow="base"
+            background="#ffffff4d"
+            p={2}
+            justifyContent="center"
+            alignItems="center"
+            borderRadius="lg"
+            mx={2}
           >
-            {t('title')}
-          </Text>
-          <NextLink href="/services">
-            <Button variant="outline" mt="0.5em" color="black" colorScheme="blue" size="sm">
-              {t('explore')}
-            </Button>
-          </NextLink>
+            <Heading
+              fontSize={{ base: '1.5em', md: '1.75em' }}
+              padding="0 0.5em"
+              textTransform="uppercase"
+              color="gray.700"
+              textAlign="center"
+            >
+              {t('title')}
+            </Heading>
+            <NextLink href="/services">
+              <Button
+                variant="outline"
+                mt="0.5em"
+                color="gray.700"
+                colorScheme="blue"
+                size="sm"
+                width={200}
+              >
+                {t('explore')}
+              </Button>
+            </NextLink>
+          </Flex>
         </Flex>
       </Box>
       <Flex flexDirection="column" alignItems="center" justifyContent="center" margin="auto">
