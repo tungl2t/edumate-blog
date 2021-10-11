ac149c1ba776bd4868682b263fb59b089ba20acd
TungLT <letungl2t@gmail.com>
2021-10-11 20:20:16 +0700

    Update custom header

--- src/lib/api/user-evaluation-tracking.api.ts
+++ src/lib/api/user-evaluation-tracking.api.ts
@@ -1,9 +1,9 @@
 import edumate from './cms-edumate-vn.api';
 
-export const findByEvaluationId = async (accessToken: string, evaluationId: number) => {
+export const findByEvaluationId = async (xAPIKey: string, evaluationId: number) => {
   const response = await edumate.get('/user-evaluation-trackings', {
     headers: {
-      access_token: accessToken,
+      'x-api-key': xAPIKey,
     },
     params: {
       evaluationId,
